4401936e59d7b9758193e80f484da883f44fd908
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-09-19 23:19:59 +0300

    Modularity ++ 👩‍🔧

--- src/manual_blur.py
+++ src/manual_blur.py
@@ -5,30 +5,29 @@ import argparse
 import cv2 as cv
 
 
-def blurBoxes(boxes):
-    pass
+def blurBoxes(image, boxes):
+    for box in boxes:
+        x,y,w,h = [d for d in box]
+        sub = image[y:y+h, x:x+w]
+        blur = cv.GaussianBlur(sub, (23,23), 30)
+        image[y:y+h, x:x+w] = blur
+    return image
 
 
 def main(args):
-    print(args)
     image = cv.imread(args.input_image)
     temp_image = image.copy()
     ROIs = []
     while True:
         box = cv.selectROI('blur', temp_image, fromCenter=False)
         ROIs.append(box)
-        #print(box)
-        cv.rectangle(temp_image, (box[0],box[1]), (box[0]+box[2], box[1]+box[3]), (0,0,255), 3)
+        cv.rectangle(temp_image, (box[0],box[1]), (box[0]+box[2], box[1]+box[3]), (0,255,0), 3)
+        print('ROI is saved, press q to stop capturing, press any other key to select other ROI')
         key = cv.waitKey(0)
         if key & 0xFF == ord('q'):
             break
-    print(ROIs)   
         
-    for box in ROIs:
-        x,y,w,h = [d for d in box]
-        sub = image[y:y+h, x:x+w]
-        blur = cv.GaussianBlur(sub, (23,23), 30)
-        image[y:y+h, x:x+w] = blur
+    image = blurBoxes(image, ROIs)
 
     if args.output_image:
         cv.imwrite(args.output_image,image)
