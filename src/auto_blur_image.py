19a7f5a62be0d6c0c73b2f3096589b6e314c0261
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-09-21 12:25:50 +0300

    Formatting 🤗

--- src/auto_blur_image.py
+++ src/auto_blur_image.py
@@ -7,10 +7,11 @@ import tensorflow as tf
 import numpy as np
 import time
 
+
 class DetectorAPI:
     def __init__(self, path_to_ckpt):
         self.path_to_ckpt = path_to_ckpt
- 
+
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
@@ -18,10 +19,10 @@ class DetectorAPI:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
- 
+
         self.default_graph = self.detection_graph.as_default()
         self.sess = tf.Session(graph=self.detection_graph)
- 
+
         # Definite input and output Tensors for detection_graph
         self.image_tensor = self.detection_graph.get_tensor_by_name(
             'image_tensor:0')
@@ -36,34 +37,38 @@ class DetectorAPI:
             'detection_classes:0')
         self.num_detections = self.detection_graph.get_tensor_by_name(
             'num_detections:0')
- 
+
     def processFrame(self, image):
         # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image, axis=0)
         # Actual detection.
         start_time = time.time()
-        (boxes, scores, classes, num) = self.sess.run(
-            [self.detection_boxes, self.detection_scores,
-                self.detection_classes, self.num_detections],
-            feed_dict={self.image_tensor: image_np_expanded})
+        (boxes, scores, classes,
+         num) = self.sess.run([
+             self.detection_boxes, self.detection_scores,
+             self.detection_classes, self.num_detections
+         ],
+                              feed_dict={self.image_tensor: image_np_expanded})
         end_time = time.time()
- 
-        print("Elapsed Time:", end_time-start_time)
- 
+
+        print("Elapsed Time:", end_time - start_time)
+
         im_height, im_width, _ = image.shape
         boxes_list = [None for i in range(boxes.shape[1])]
         for i in range(boxes.shape[1]):
             boxes_list[i] = (int(boxes[0, i, 1] * im_width),
-                            int(boxes[0, i, 0] * im_height),
+                             int(boxes[0, i, 0] * im_height),
                              int(boxes[0, i, 3] * im_width),
-                             int(boxes[0, i, 2] * im_height)
-                             )
- 
-        return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
- 
+                             int(boxes[0, i, 2] * im_height))
+
+        return boxes_list, scores[0].tolist(), [
+            int(x) for x in classes[0].tolist()
+        ], int(num[0])
+
     def close(self):
         self.sess.close()
         self.default_graph.close()
+
 
 def blurBoxes(image, boxes):
     """
@@ -74,20 +79,20 @@ def blurBoxes(image, boxes):
     Returns:
     image -- the blurred image as a matrix
     """
-    
+
     for box in boxes:
         # unpack each box
-        x1,y1,x2,y2 = [d for d in box]
-        
+        x1, y1, x2, y2 = [d for d in box]
+
         # crop the image due to the current box
         sub = image[y1:y2, x1:x2]
-        
+
         # apply GaussianBlur on cropped area
-        blur = cv.blur(sub, (25,25))
-        
+        blur = cv.blur(sub, (25, 25))
+
         # paste blurred image on the original image
         image[y1:y2, x1:x2] = blur
-        
+
     return image
 
 
@@ -98,54 +103,64 @@ def main(args):
 
     # create detection object
     odapi = DetectorAPI(path_to_ckpt=model_path)
-        
+
     # open image
     image = cv.imread(args.input_image)
-    
+
     # real face detection
     boxes, scores, classes, num = odapi.processFrame(image)
-    
+
     # filter boxes due to threshold
     # boxes are in (x_top_left, y_top_left, x_bottom_right, y_bottom_right) format
     boxes = [boxes[i] for i in range(0, num) if scores[i] > threshold]
-    
+
     # apply blurring
     image = blurBoxes(image, boxes)
-    
-    # show image 
-    cv.imshow('blurred',image)
-    
+
+    # show image
+    cv.imshow('blurred', image)
 
     # if image will be saved then save it
     if args.output_image:
-        cv.imwrite(args.output_image,image)
-        print('Image has been saved at', args.output_image, ' path')
-    cv.imshow('blurred',image)
+        cv.imwrite(args.output_image, image)
+        print('Image has been saved successfully at', args.output_image,
+              'path')
+    cv.imshow('blurred', image)
 
     # when any key has been pressed then close window and stop the program
     cv.waitKey(0)
     cv.destroyAllWindows()
-    
-    
+
 
 if __name__ == "__main__":
     # creating argument parser
     parser = argparse.ArgumentParser(description='Image blurring parameters')
-    
+
     # adding arguments
-    parser.add_argument('-i', '--input_image',
-                        help='Path to your image', type=str, required=True)
-    parser.add_argument('-m', '--model_path',
-                        help='Path to .pb model', type=str, required=True)
-    parser.add_argument('-o', '--output_image',
-                        help='Output file path', type=str)
-    parser.add_argument('-t', '--threshold',
-                        help='Face detection confidence', default=0.7, type=float)
+    parser.add_argument('-i',
+                        '--input_image',
+                        help='Path to your image',
+                        type=str,
+                        required=True)
+    parser.add_argument('-m',
+                        '--model_path',
+                        help='Path to .pb model',
+                        type=str,
+                        required=True)
+    parser.add_argument('-o',
+                        '--output_image',
+                        help='Output file path',
+                        type=str)
+    parser.add_argument('-t',
+                        '--threshold',
+                        help='Face detection confidence',
+                        default=0.7,
+                        type=float)
     args = parser.parse_args()
     print(args)
     # if input image path is invalid then stop
     assert os.path.isfile(args.input_image), 'Invalid input file'
-    
+
     # if output directory is invalid then stop
     if args.output_image:
         assert os.path.isdir(os.path.dirname(
