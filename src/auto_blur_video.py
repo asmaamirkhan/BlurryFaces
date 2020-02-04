10f1e5613fe59c3ef6d3a06974be82bcc7b5fc14
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-04 17:42:48 +0300

    More modular structure 👩‍🔧

--- src/auto_blur_video.py
+++ src/auto_blur_video.py
@@ -3,79 +3,14 @@
 import os
 import argparse
 import cv2 as cv
-import tensorflow as tf
-import numpy as np
-import time
-
-
-class DetectorAPI:
-    def __init__(self, path_to_ckpt):
-        self.path_to_ckpt = path_to_ckpt
-
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
-            od_graph_def = tf.compat.v1.GraphDef()
-            with tf.io.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
-
-        self.default_graph = self.detection_graph.as_default()
-        self.sess = tf.compat.v1.Session(graph=self.detection_graph)
-
-        # Definite input and output Tensors for detection_graph
-        self.image_tensor = self.detection_graph.get_tensor_by_name(
-            'image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
-        self.detection_boxes = self.detection_graph.get_tensor_by_name(
-            'detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        self.detection_scores = self.detection_graph.get_tensor_by_name(
-            'detection_scores:0')
-        self.detection_classes = self.detection_graph.get_tensor_by_name(
-            'detection_classes:0')
-        self.num_detections = self.detection_graph.get_tensor_by_name(
-            'num_detections:0')
-
-    def processFrame(self, image):
-        # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image, axis=0)
-        # Actual detection.
-        start_time = time.time()
-        (boxes, scores, classes,
-         num) = self.sess.run([
-             self.detection_boxes, self.detection_scores,
-             self.detection_classes, self.num_detections
-         ],
-                              feed_dict={self.image_tensor: image_np_expanded})
-        end_time = time.time()
-
-        print("Elapsed Time:", end_time - start_time)
-
-        im_height, im_width, _ = image.shape
-        boxes_list = [None for i in range(boxes.shape[1])]
-        for i in range(boxes.shape[1]):
-            boxes_list[i] = (int(boxes[0, i, 1] * im_width),
-                             int(boxes[0, i, 0] * im_height),
-                             int(boxes[0, i, 3] * im_width),
-                             int(boxes[0, i, 2] * im_height))
-
-        return boxes_list, scores[0].tolist(), [
-            int(x) for x in classes[0].tolist()
-        ], int(num[0])
-
-    def close(self):
-        self.sess.close()
-        self.default_graph.close()
-
+from DetectorAPI import DetectorAPI
 
 def blurBoxes(image, boxes):
     """
     Argument:
     image -- the image that will be edited as a matrix
     boxes -- list of boxes that will be blurred, each box must be int the format (x_top_left, y_top_left, x_bottom_right, y_bottom_right)
-    
+
     Returns:
     image -- the blurred image as a matrix
     """
@@ -113,7 +48,8 @@ def main(args):
 
     if args.output_video:
         fourcc = cv.VideoWriter_fourcc(*'mp4v')
-        output = cv.VideoWriter(args.output_video, fourcc, 20.0, (int(capture.get(3)), int(capture.get(4))))
+        output = cv.VideoWriter(args.output_video, fourcc,
+                                20.0, (int(capture.get(3)), int(capture.get(4))))
 
     frame_counter = 0
     while True:
@@ -145,7 +81,7 @@ def main(args):
         if args.output_video:
             output.write(frame)
             print('Blurred video has been saved successfully at',
-                args.output_video, 'path')
+                  args.output_video, 'path')
 
     # when any key has been pressed then close window and stop the program
 
